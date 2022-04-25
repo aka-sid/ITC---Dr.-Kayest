a66cdcba6a42dea3b0c30afd57bfdd498facef30
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-04-25 10:22:20 +0530

    Delete Q3.py

--- Q3.py
+++
@@ -1,8 +0,0 @@
-sid=int(input("Enter SID "))
-name=str(input("Enter Name "))
-gender=str(input("Enter Gender "))
-course=str(input("Enter Course Name "))
-gpa=float(input("Enter CGPA "))
-
-L=[sid,name,gender,course,gpa]
-print (L)
