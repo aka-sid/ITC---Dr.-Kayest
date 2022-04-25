cf4262c38a4241902419a944c11168464dc87362
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-04-25 09:56:06 +0530

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,9 @@
+m1=int(input("Enter first marks "))
+m2=int(input("Enter second marks "))
+m3=int(input("Enter three marks "))
+m4=int(input("Enter fourth marks "))
+m5=int(input("Enter fifth marks "))
+
+L=[m1,m2,m3,m4,m5]
+L.sort()
+print(L)
