1ba0bc50d26ae6b8371abb770f653dd5ae060b22
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-04-25 09:55:45 +0530

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,8 @@
+sid=int(input("Enter SID "))
+name=str(input("Enter Name "))
+gender=str(input("Enter Gender "))
+course=str(input("Enter Course Name "))
+gpa=float(input("Enter CGPA "))
+
+L=[sid,name,gender,course,gpa]
+print (L)
