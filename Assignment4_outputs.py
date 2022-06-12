cc97092ea7355f7522f5768801c1b9100bd9e442
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-06-12 14:41:50 +0530

    Create Assignment4_outputs.py

---
+++ Assignment4_outputs.py
@@ -0,0 +1,30 @@
+
+#Q1
+
+Enter the string to be reversed
+Siddharth
+htrahddiS
+
+#Q2
+
+Enter starting of Range
+5
+Enter end of Range
+105
+Enter the number
+7
+Numbers in range  5  to  105  divisible by  7  are:
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+77
+84
+91
+98
