e1836e7e2a1b19c8a71994b148dea69ad99ab655
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-06-18 22:57:20 +0530

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -65,4 +65,116 @@ while i<=10:
 		print(str)
 		i+=1
 		
+#Q5
+
+rows=int(input("Enter the no of rows: ")) 
+m=65
+for i in range(rows):
+    for j in range(i+1):
+        if m>90:
+            m=65
+        print(chr(m),end='')
+        m+=1 
+    print()
+
+
+
+
+#Q6
+
+lower_value = int(input ("Please, Enter the Lowest Range Value : \n"))  
+upper_value = int(input ("Please, Enter the Upper Range Value : \n"))  
+a = 1
+
+print()
+
+print ("The Prime Numbers in the range are: ")  
+for number in range (lower_value, upper_value + 1):  
+    if number > 1:  
+        for i in range (2, number):  
+            if (number % i) == 0:  
+                break  
+        else:  
+            print (a,")",number)  
+            a += 1
+
 		
+	
+	
+	
+	
+#Q7
+
+for i in range(1,500):
+    if i%7==0 and i%11==0:
+        print(i)
+
+
+	
+	
+	
+	
+#Q8	
+list=[]
+for i in range(0,10,1):
+    n=int(input(f"Enter {i+1}th number: "))
+    list.append(n)
+print(list)
+
+#a)
+print("Positive numbers are: ")
+for i in range(10):
+    if list[i]>0:
+        print(list[i])
+
+#b)
+print("Negative numbers are: ")
+for i in range(10):
+    if list[i]<0:
+        print(list[i])
+
+#c)
+print("Odd numbers are: ")
+for i in range(10):
+    if list[i]%2!=0:
+        print(list[i])
+
+#d)
+print("Even numbers are: ")
+for i in range(10):
+    if list[i]%2==0:
+        print(list[i])
+
+#e)
+count=dict()
+for no in list:
+    if no in count:
+        count[no]+=1
+    else:
+        count[no]=1
+
+print("No of times each number occurs in the List:")
+print(count)
+
+
+
+
+
+
+
+
+#Q9
+
+str=input("Enter a string: ")
+#Empty dictionary is created to 
+counts = dict()
+#We split the input string into words and store it in in a list
+words = str.split(' ')
+for word in words:
+    if word in counts:
+        counts[word] += 1
+    else:
+        counts[word] = 1
+
+print(counts)
+
