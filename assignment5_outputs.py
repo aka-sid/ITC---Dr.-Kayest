42e6cae32994a91cd7d8982b81f7e791496ff191
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-06-18 23:01:42 +0530

    Update assignment5_outputs.py

--- assignment5_outputs.py
+++ assignment5_outputs.py
@@ -53,3 +53,85 @@ Area of the triangle is:  6.0
 ***
 **
 *
+
+
+#Q5
+
+Enter the no of rows: 10
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+CDEFGHIJ
+KLMNOPQRS
+TUVWXYZABC
+
+#Q6
+
+Please, Enter the Lowest Range Value : 
+1
+Please, Enter the Upper Range Value : 
+10
+
+The Prime Numbers in the range are: 
+1 ) 2
+2 ) 3
+3 ) 5
+4 ) 7
+
+#Q7
+
+77
+154
+231
+308
+385
+462
+
+#Q8
+
+Enter 1th number: 10
+Enter 2th number: 20
+Enter 3th number: 30
+Enter 4th number: 40
+Enter 5th number: 50
+Enter 6th number: 60
+Enter 7th number: 70
+Enter 8th number: 80
+Enter 9th number: 90
+Enter 10th number: 100
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+Positive numbers are: 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+Negative numbers are: 
+Odd numbers are: 
+Even numbers are: 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+No of times each number occurs in the List:
+{10: 1, 20: 1, 30: 1, 40: 1, 50: 1, 60: 1, 70: 1, 80: 1, 90: 1, 100: 1}
+
+#Q9
+
+Enter a string: I love music
+{'I': 1, 'love': 1, 'music': 1}
