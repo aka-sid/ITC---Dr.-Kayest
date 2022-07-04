ede0d4d710b74c080f9fb830da6d51df8a13b190
aka-sid <90271633+aka-sid@users.noreply.github.com>
2022-07-04 13:38:49 +0530

    Create assignment6_outputs.py

---
+++ assignment6_outputs.py
@@ -0,0 +1,68 @@
+Enter a number: 28
+28  is a perfect number
+
+
+
+Enter a String: Madam
+Madam is not a palindrome
+
+
+
+
+Enter a number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+  
+  
+  
+  
+
+Enter a String: I like to play games
+No 'I like to play games' is not a panagram
+
+
+Enter a hyphen separated sentence: hobby-singing
+hobby-singing
+
+
+
+Student ID= 21102096
+Student ID= 21102096
+Student Name= Siddharth Kulshrestha
+Student ID= 21102096
+Student Name= Siddharth Kulshrestha
+Student Class= 1st Year
+
+
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+
+
+
+
+Enter number of elements : 6
+14
+-10
+-4
+440
+-399
+-42
+14 -10 -4
+None
+
+
+
+
+Enter the sequence of parantheses : (){}[]
+(){}[] - is balanced
